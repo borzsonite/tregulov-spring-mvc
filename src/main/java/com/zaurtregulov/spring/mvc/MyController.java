336ade66b85d324cd34da9e9b96dce2d9e155a53
Sergey Rozhkov <borzsonite@gmail.com>
2021-05-31 13:02:53 +0300

    62 start

--- src/main/java/com/zaurtregulov/spring/mvc/MyController.java
+++ src/main/java/com/zaurtregulov/spring/mvc/MyController.java
@@ -1,9 +1,14 @@
 package com.zaurtregulov.spring.mvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
+@RequestMapping("/employee") // ко всем методам контролера в @RequestMapping добавиться /employee
 public class MyController {
 
     @RequestMapping("/")
@@ -16,8 +21,25 @@ public class MyController {
         return "ask-emp-details-view";
     }
 
-    @RequestMapping("showDetails")
-    public String showDetails () {
+//    @RequestMapping("/showDetails")
+//    public String showDetails () {
+//        return "show-emp-details-view";
+//    }
+
+//    @RequestMapping("/showDetails")
+//    public String showDetails (HttpServletRequest request, Model model) { // получение данных из запроса добавление их в модель и передача во view
+//        String empName = request.getParameter("employeeName"); // получили данные
+//        empName = "Mr. " + empName;
+//        model.addAttribute("empName", empName); // добавили в модель
+//        model.addAttribute("descripiton", " nice to see you!");
+//        return "show-emp-details-view";
+//    }
+
+    @RequestMapping("/showDetails")
+    public String showDetails (@RequestParam("employeeName") String requestParam, Model model) { // получение данных путем  @RequestParam и добавление их в модель и передача во view
+        requestParam = "MR. " + requestParam;
+        model.addAttribute("empName", requestParam); // добавили в модель
+        model.addAttribute("descripiton", " Nice to see you!");
         return "show-emp-details-view";
     }
 
--- src/main/webapp/WEB-INF/view/first-view.jsp
+++ src/main/webapp/WEB-INF/view/first-view.jsp
@@ -2,6 +2,6 @@
 <body>
 <h2>Good luck!</h2>
 
-<a href="/askDetails">Ask details</a>
+<a href="askDetails">Ask details</a>
 </body>
 </html>
